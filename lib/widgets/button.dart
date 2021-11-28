f679ddc13bd3094f5c55593e6a07d709a4f5671c
nadiamrnt <nadiamrnt@gmail.com>
2021-11-28 22:34:23 +0700

    menambah fungsi ontap pada button

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -3,14 +3,19 @@ import 'package:peka/common/styles.dart';
 
 class Button extends StatelessWidget {
   final String textButton;
+  // ignore: prefer_typing_uninitialized_variables
+  final pindahHalaman;
 
   // ignore: use_key_in_widget_constructors
-  const Button({required this.textButton});
+  const Button({required this.textButton, required this.pindahHalaman});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => pindahHalaman));
+      },
       child: ConstrainedBox(
         constraints: const BoxConstraints.tightForFinite(),
         child: Container(
