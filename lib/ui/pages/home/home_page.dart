a685e55e645614eb33d967d34ea020825534750f
rrdhoi <r.ridoi702@gmail.com>
2021-12-01 17:28:04 +0800

    menambahkan halaman Home

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:peka/common/styles.dart';
+import 'package:peka/utils/category_helper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,14 +9,474 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 30.0, left: 24, right: 24),
+      body: SafeArea(
+        child: SingleChildScrollView(
           child: Column(
-            children: [],
+            children: [
+              _buildHeader(),
+              const SizedBox(height: 30),
+              _buildSearch(),
+              const SizedBox(height: 24),
+              _buildCategory(),
+              const SizedBox(height: 24),
+              _buildListPantiAsuhan(),
+              const SizedBox(height: 30),
+            ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: 30,
+        left: defaultMargin,
+        right: defaultMargin,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  'Halo,\nJustin Bieber',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: semiBold,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Container(
+                width: 60,
+                height: 60,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                      'assets/images/img_profile.png',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Text(
+            'Mau donasi apa hari ini?',
+            style: greyTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: light,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearch() {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: defaultMargin,
+        right: defaultMargin,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: SizedBox(
+              height: 45,
+              child: TextField(
+                style: blackTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: regular,
+                ),
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  hintText: 'Cari Panti Asuhan',
+                  hintStyle: greyTextStyle.copyWith(
+                    fontSize: 14,
+                    fontWeight: regular,
+                  ),
+                  contentPadding: const EdgeInsets.only(right: 8, left: 12),
+                  filled: true,
+                  fillColor: kWhiteBgColor,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 14.0,
+                      right: 18,
+                    ),
+                    child: Image.asset(
+                      'assets/icons/ic_search.png',
+                      width: 24,
+                    ),
+                  ),
+                  prefixIconConstraints: const BoxConstraints(
+                    minHeight: 24,
+                    minWidth: 24,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 6),
+          Container(
+            height: 45,
+            width: 47,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Image.asset(
+              'assets/icons/ic_filter.png',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCategory() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: Text(
+            'Kategori',
+            style: blackTextStyle.copyWith(fontWeight: semiBold, fontSize: 16),
+          ),
+        ),
+        const SizedBox(height: 16),
+        SizedBox(
+          height: 75,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 24),
+            children: CategoryHelper.categoryFromLocal.map((item) {
+              return Padding(
+                padding: const EdgeInsets.only(right: 18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 45,
+                      height: 45,
+                      padding: const EdgeInsets.all(7),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: CategoryHelper.categoryFromLocal
+                                .indexOf(item)
+                                .isEven
+                            ? kBlueBgColor
+                            : kPinkBgColor,
+                      ),
+                      child: Image.asset(
+                        item['image'],
+                        width: 30,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Text(
+                      item['name'],
+                      style: greyTextStyle.copyWith(
+                        fontWeight: regular,
+                        fontSize: 10,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildListPantiAsuhan() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: defaultMargin),
+          child: Text(
+            'Rekomendasi',
+            style: blackTextStyle.copyWith(
+              fontWeight: semiBold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        SizedBox(
+          height: 330,
+          child: ListView(
+            padding: const EdgeInsets.only(left: 24),
+            scrollDirection: Axis.horizontal,
+            children: [
+              Container(
+                height: 324,
+                width: 235,
+                margin: const EdgeInsets.only(right: 24),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: kWhiteBgColor,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 224,
+                      width: 215,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        image: const DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage('assets/images/img_house.png'),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Panti Asuhan Al-Khaer',
+                            style: blackTextStyle.copyWith(
+                                fontSize: 16,
+                                fontWeight: medium,
+                                overflow: TextOverflow.ellipsis),
+                          ),
+                          const SizedBox(height: 6),
+                          Row(
+                            children: [
+                              Image.asset(
+                                'assets/icons/ic_location.png',
+                                height: 14,
+                                width: 11,
+                              ),
+                              const SizedBox(width: 7),
+                              Text(
+                                'Makassar',
+                                style: greyTextStyle.copyWith(
+                                  fontSize: 14,
+                                  fontWeight: light,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 324,
+                width: 235,
+                margin: const EdgeInsets.only(right: 24),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: kWhiteBgColor,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 224,
+                      width: 215,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        image: const DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage('assets/images/img_house.png'),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Panti Asuhan Al-Khaer',
+                            style: blackTextStyle.copyWith(
+                                fontSize: 16,
+                                fontWeight: medium,
+                                overflow: TextOverflow.ellipsis),
+                          ),
+                          const SizedBox(height: 6),
+                          Row(
+                            children: [
+                              Image.asset(
+                                'assets/icons/ic_location.png',
+                                height: 14,
+                                width: 11,
+                              ),
+                              const SizedBox(width: 7),
+                              Text(
+                                'Makassar',
+                                style: greyTextStyle.copyWith(
+                                  fontSize: 14,
+                                  fontWeight: light,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 324,
+                width: 235,
+                margin: const EdgeInsets.only(right: 24),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: kWhiteBgColor,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 224,
+                      width: 215,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        image: const DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage('assets/images/img_house.png'),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Panti Asuhan Al-Khaer',
+                            style: blackTextStyle.copyWith(
+                                fontSize: 16,
+                                fontWeight: medium,
+                                overflow: TextOverflow.ellipsis),
+                          ),
+                          const SizedBox(height: 6),
+                          Row(
+                            children: [
+                              Image.asset(
+                                'assets/icons/ic_location.png',
+                                height: 14,
+                                width: 11,
+                              ),
+                              const SizedBox(width: 7),
+                              Text(
+                                'Makassar',
+                                style: greyTextStyle.copyWith(
+                                  fontSize: 14,
+                                  fontWeight: light,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 324,
+                width: 235,
+                margin: const EdgeInsets.only(right: 24),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: kWhiteBgColor,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 224,
+                      width: 215,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        image: const DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage('assets/images/img_house.png'),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Panti Asuhan Al-Khaer',
+                            style: blackTextStyle.copyWith(
+                                fontSize: 16,
+                                fontWeight: medium,
+                                overflow: TextOverflow.ellipsis),
+                          ),
+                          const SizedBox(height: 6),
+                          Row(
+                            children: [
+                              Image.asset(
+                                'assets/icons/ic_location.png',
+                                height: 14,
+                                width: 11,
+                              ),
+                              const SizedBox(width: 7),
+                              Text(
+                                'Makassar',
+                                style: greyTextStyle.copyWith(
+                                  fontSize: 14,
+                                  fontWeight: light,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/ui/widgets/button.dart
+++ lib/ui/widgets/button.dart
@@ -3,12 +3,10 @@ import 'package:peka/common/styles.dart';
 
 class Button extends StatelessWidget {
   final String textButton;
-
-  // ignore: prefer_typing_uninitialized_variables
   final Function() onTap;
 
-  // ignore: use_key_in_widget_constructors
-  const Button({required this.textButton, required this.onTap});
+  const Button({required this.textButton, required this.onTap, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +14,6 @@ class Button extends StatelessWidget {
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
         primary: kPrimaryColor,
-        padding: const EdgeInsets.symmetric(vertical: 18.0, horizontal: 55),
         minimumSize: const Size(double.infinity, 55),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(18),
